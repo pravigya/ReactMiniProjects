51b533f97fda97002dda0f40aca51f8880280c41
pravigya <pravigyajain5@gmail.com>
2023-06-21 22:24:48 +0530

    conditional rendering

--- StopWatch/src/App.jsx
+++ StopWatch/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-//import "./App.css";
+import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(false);
+  const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
 
   useEffect(() => {
@@ -16,25 +16,29 @@ function App() {
     } else if (!running) {
       clearInterval(interval);
     }
+    return () => clearInterval(interval);
   }, [running]);
 
   return (
     <>
-      <h1>StopWatch</h1>
+      <h1 className="Heading">StopWatch</h1>
       <div>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </div>
       <div>
-        <button
-          onClick={() => {
-            setRunning(true);
-          }}
-        >
-          Start
-        </button>
-        <button onClick={() => setRunning(false)}>Stop</button>
+        {!running ? (
+          <button
+            onClick={() => {
+              setRunning(true);
+            }}
+          >
+            Start
+          </button>
+        ) : (
+          <button onClick={() => setRunning(false)}>Stop</button>
+        )}
         <button onClick={() => setTime(0)}>Reset</button>
       </div>
     </>
